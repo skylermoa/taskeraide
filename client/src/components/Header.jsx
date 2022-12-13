8ef0f8869cf4338c2ae05b0faeddedfded170f91
Skyler Moa <skylermoa@gmail.com>
2022-12-13 12:07:07 -0700

    fixed disappearing header links

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,12 +17,12 @@ function Header() {
     <header className="container">
       <div className="row">
         <div className="col-12">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <nav className="navbar navbar-expand-sm navbar-light bg-light">
             <div className="container-fluid">
               <Link className="navbar-brand" to="/">
                 <span className="ms-2">TaskerAide</span>
               </Link>
-              <div className="collapse navbar-collapse" id="navbarNav">
+              <div className="navbar-collapse">
                 <ul className="navbar-nav ms-auto">
                   {user ? (
                     <>
